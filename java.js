5fb40be65b8c3f39147cd41a686c87b64e4ed978
maram121‏ <maram_mohra@hotmail.com>
2021-04-22 10:49:29 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,16 @@
+let a = prompt("enter a string");
+switch (a)
+{case "red":
+alert("apple");
+break;
+case "yellow":
+alert("banana");
+break;
+case "circle":
+case "rectangle":
+case "square":
+alert("shape");
+break;
+default:
+alert("i don't know");
+}
